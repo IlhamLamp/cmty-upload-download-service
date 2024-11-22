78a9785e008f5d5160bedeee8f1662b4c4f431f9
IlhamLamp <ilhamutomo24@gmail.com>
2024-11-23 06:44:32 +0700

    add: license | fix: load env with array string

--- config/config.go
+++ config/config.go
@@ -8,11 +8,12 @@ import (
 )
 
 type Config struct {
+	AppPort             string
 	CloudinaryApiKey    string
 	CloudinaryApiSecret string
 	CloudinaryCloudName string
 	JwtAccessSecret     string
-	RabbitMQUrl string
+	RabbitMQUrl         string
 }
 
 func LoadConfig() Config {
@@ -21,18 +22,38 @@ func LoadConfig() Config {
 		log.Fatalf("Error loading env file")
 	}
 
+	requiredEnv := []string{
+		"APP_PORT",
+		"CLOUDINARY_API_KEY",
+		"CLOUDINARY_API_SECRET",
+		"CLOUDINARY_CLOUD_NAME",
+		"JWT_ACCESS_SECRET",
+		"RABBITMQ_URL",
+	}
+
+	missingEnv := checkRequiredEnv(requiredEnv)
+	if len(missingEnv) > 0 {
+		log.Fatalf("Missing required environment variables: %v", missingEnv)
+	}
+
 	config := Config{
+		AppPort:             os.Getenv("APP_PORT"),
 		CloudinaryApiKey:    os.Getenv("CLOUDINARY_API_KEY"),
 		CloudinaryApiSecret: os.Getenv("CLOUDINARY_API_SECRET"),
 		CloudinaryCloudName: os.Getenv("CLOUDINARY_CLOUD_NAME"),
 		JwtAccessSecret:     os.Getenv("JWT_ACCESS_SECRET"),
-		RabbitMQUrl: os.Getenv("RABBITMQ_URL"),
-	}
-
-	if config.CloudinaryApiKey == "" || config.CloudinaryApiSecret == "" ||
-		config.CloudinaryCloudName == "" || config.JwtAccessSecret == "" {
-		log.Fatal("Required environment variable missing")
+		RabbitMQUrl:         os.Getenv("RABBITMQ_URL"),
 	}
 
 	return config
+}
+
+func checkRequiredEnv(keys []string) []string {
+	var missing []string
+	for _, key := range keys {
+		if os.Getenv(key) == "" {
+			missing = append(missing, key)
+		}
+	}
+	return missing
 }
