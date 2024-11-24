15eba43cd28882cade65c6732d3e5f423d598c84
IlhamLamp <ilhamutomo24@gmail.com>
2024-11-24 22:08:15 +0700

    fix: upload image

--- handlers/upload.go
+++ handlers/upload.go
@@ -36,13 +36,13 @@ func extractPublicID(oldImageURL string) (string, error) {
 func UploadFileHandler(cldClient *utils.CloudinaryClient, mqClient *utils.RabbitMQClient) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		log.Println("Start handling file upload request")
+
 		file, err := c.FormFile("file")
 		if err != nil {
 			log.Printf("Error fetching file from request: %v", err)
 			respondWithError(c, http.StatusBadRequest, "File is required!")
 			return
 		}
-		log.Printf("File received: %s", file.Filename)
 
 		openedFile, err := file.Open()
 		if err != nil {
@@ -52,10 +52,8 @@ func UploadFileHandler(cldClient *utils.CloudinaryClient, mqClient *utils.Rabbit
 		}
 		defer openedFile.Close()
 
-		fileName := file.Filename
-
 		log.Println("Uploading file to Cloudinary...")
-		url, err := cldClient.UploadFile(openedFile, fileName)
+		url, err := cldClient.UploadFile(openedFile, file.Filename)
 		if err != nil {
 			log.Printf("Error uploading to Cloudinary: %v", err)
 			respondWithError(c, http.StatusInternalServerError, "Failed to upload into cloudinary")
@@ -64,18 +62,19 @@ func UploadFileHandler(cldClient *utils.CloudinaryClient, mqClient *utils.Rabbit
 		log.Printf("File uploaded successfully to: %s", url)
 
 		oldImageURL := c.PostForm("old_image_url")
-
 		if oldImageURL != "" {
-			log.Printf("Old image URL: %s", oldImageURL)
-			if publicId, err := extractPublicID(oldImageURL); err == nil {
-				log.Printf("Public ID: %s", publicId)
+			log.Printf("Processing old image URL: %s", oldImageURL)
+			publicId, err := extractPublicID(oldImageURL)
+			if err != nil {
+				log.Printf("Failed to extract public ID from URL: %v", err)
+			} else {
+				log.Printf("Public ID extracted: %s", publicId)
 				if err := mqClient.PublishDeleteImageMessage(publicId); err != nil {
-					log.Printf("Error published delete image: %s", err)
+					log.Printf("Failed to queue old image deletion for public ID %s: %v", publicId, err)
 					respondWithError(c, http.StatusInternalServerError, "Failed to queue old image for deletion")
 					return
 				}
-			} else {
-				log.Printf("Error extracting public ID: %v", err)
+				log.Printf("Old image queued for deletion: %s", publicId)
 			}
 		}
 
